3337b4eca4f3acac302561e16ef665ee26220434
Joabe Morais <joabealvesmorais@gmail.com>
2025-06-10 04:47:09 -0300

    config: add tests for "get_successors_of_state"

--- tests/core/domain/test_models.py
+++ tests/core/domain/test_models.py
@@ -80,3 +80,52 @@ def test_get_state_by_tonal_function_multiple_matches_returns_one(kripke_config_
     assert tonic_state is not None, "A tonic state should be found"
     assert tonic_state.associated_tonal_function == TonalFunction.TONIC, "Returned state must have TONIC function"
     assert tonic_state == sample_states["s_t"] or tonic_state == sample_states["s_t2"], "Returned state must be one of the known tonic states"
+
+# --- Tests for get_successors_of_state ---
+
+def test_get_successors_of_sd(kripke_config_populated: KripkeStructureConfig, sample_states: Dict):
+    """Test successors for s_sd (Subdominant), which has multiple distinct successors."""
+    successors_of_ssd = kripke_config_populated.get_successors_of_state(sample_states["s_sd"])
+    expected_successors = {sample_states["s_d"], sample_states["s_t"]}
+    assert set(successors_of_ssd) == expected_successors, "Successors of s_sd are incorrect"
+
+def test_get_successors_of_sd_single_successor(kripke_config_populated: KripkeStructureConfig, sample_states: Dict):
+    """Test s_d (Dominant), which has exactly one successor."""
+    successors_of_sd = kripke_config_populated.get_successors_of_state(sample_states["s_d"])
+    expected_successors = {sample_states["s_t"]}
+    assert set(successors_of_sd) == expected_successors, "Successor of s_d should be s_t"
+
+def test_get_successors_of_st_multiple_successors(kripke_config_populated: KripkeStructureConfig, sample_states: Dict):
+    """Test s_t (Tonic), which has multiple successors."""
+    successors_of_st = kripke_config_populated.get_successors_of_state(sample_states["s_t"])
+    expected_successors = {sample_states["s_sd"], sample_states["s_d"]}
+    assert set(successors_of_st) == expected_successors, "Successors of s_t are incorrect"
+
+def test_get_successors_of_st2_no_successors(kripke_config_populated: KripkeStructureConfig, sample_states: Dict):
+    """Test s_t2 (another Tonic), which has no defined successors in R."""
+    successors_of_st2 = kripke_config_populated.get_successors_of_state(sample_states["s_t2"])
+    assert successors_of_st2 == None, "s_t2 should have no successors"
+
+def test_get_successors_state_not_in_config_relations(kripke_config_populated: KripkeStructureConfig):
+    """Test querying successors for a state object that isn't a source in any relation in R."""
+    state_like_st2 = KripkeState(state_id="s_t2", associated_tonal_function=TonalFunction.TONIC)
+    successors = kripke_config_populated.get_successors_of_state(state_like_st2)
+    assert successors == None, "A state not acting as a source in R should have no successors"
+
+def test_get_successors_unknown_state(kripke_config_populated: KripkeStructureConfig):
+    """Test querying successors for a state object that isn't part of the config's states set at all."""
+    unknown_state = KripkeState(state_id="s_unknown", associated_tonal_function=TonalFunction.TONIC)
+    successors = kripke_config_populated.get_successors_of_state(unknown_state)
+    assert successors == None, "An unknown state should have no successors from the defined relations"
+
+def test_get_successors_empty_relations_config(sample_states: Dict):
+    """Test when the accessibility_relation_R is empty, but states exist."""
+    config_no_relations = KripkeStructureConfig(states=set(sample_states.values()), accessibility_relation_R=set())
+    successors = config_no_relations.get_successors_of_state(sample_states["s_t"])
+    assert successors == None, "Should be no successors if R is empty"
+
+def test_get_successors_empty_config_overall(kripke_config_empty: KripkeStructureConfig):
+    """Test get_successors_of_state on a completely empty KripkeStructureConfig."""
+    dummy_state = KripkeState(state_id="s_dummy", associated_tonal_function=TonalFunction.TONIC)
+    successors = kripke_config_empty.get_successors_of_state(dummy_state)
+    assert successors == None, "Should be no successors in an entirely empty config"
